fd32429b189e2b24f72c0426d89db97793af593f
Mursalin <mursalin2311@gmail.com>
2024-09-06 00:04:25 +0600

    updated the red tag or friend request

--- src/Components/SmallComponents/Friend/FriendRequests.jsx
+++ src/Components/SmallComponents/Friend/FriendRequests.jsx
@@ -96,7 +96,7 @@ const FriendRequests = ({ currentUser, refetchUser }) => {
         onClick={handleIconClick}
       />
       {totalFriendRequest > 0 && (
-        <div className="absolute -top-2 -right-4 flex items-center justify-center w-5 h-5 rounded-full bg-red-500 text-white text-xs">
+        <div className="absolute -top-2 -right-2 flex items-center justify-center w-5 h-5 rounded-full bg-red-500 text-white text-xs">
           {totalFriendRequest}
         </div>
       )}
