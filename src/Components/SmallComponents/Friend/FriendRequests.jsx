74b120cd7a573849d3c96f2810d924e7a6332e13
Mursalin <mursalin2311@gmail.com>
2024-09-06 00:01:43 +0600

    so i think the time is for refactor

--- src/Components/SmallComponents/Friend/FriendRequests.jsx
+++ src/Components/SmallComponents/Friend/FriendRequests.jsx
@@ -96,11 +96,11 @@ const FriendRequests = ({ currentUser, refetchUser }) => {
         onClick={handleIconClick}
       />
       {totalFriendRequest > 0 && (
-        <div className="absolute -top-1 -right-1 flex items-center justify-center w-5 h-5 rounded-full bg-primary text-white text-xs">
+        <div className="absolute -top-2 -right-4 flex items-center justify-center w-5 h-5 rounded-full bg-red-500 text-white text-xs">
           {totalFriendRequest}
         </div>
       )}
-
+ 
       {isDropdownOpen && (
         <div
           ref={dropdownRef}
