fee444d069101cf1c0b7e75a84f44204beaef7e0
Mursalin <mursalin2311@gmail.com>
2024-09-08 03:42:22 +0600

    deploying now

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "welcome-background": "url('./src/assets/welcomepagebg.webp')",
+        "chat-background": "url('./src/assets/chatpagebg.webp')",
+        "profile-background": "url('./src/assets/profile-cover-bg.webp')",
+        "people-background": "url('./src/assets/peoples-bg.webp')",
+      },
+      boxShadow: {
+        "shadow-1": "0px 0px 60px 0px rgba(0, 0, 0, 0.05)",
+      },
+      colors: {
+        primary: "#8210ed",
+        primaryDark: "#5f07b7",
+        primaryLight: "#9642e5",
+        primaryLightest: "#995bd3",
+        secondary: "#13daf4",
+        secondaryDark: "#075660",
+        secondaryLight: "#40cbdd",
+        textMain: "#727272",
+        textDark: "#16171A",
+      },
+      // Adding the dark overlay
+      backgroundColor: {
+        "dark-overlay": "rgba(0, 0, 0, 0.5)", // Adjust the opacity as needed
+      },
+    },
+  },
+  plugins: [],
+};
