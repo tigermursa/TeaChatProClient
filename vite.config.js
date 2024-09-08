81613fbd207147aeb0f4e615cac1623f056f35f4
Mursalin <mursalin2311@gmail.com>
2024-09-08 10:58:48 +0600

    updated the vite config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  
-})
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://teachat-server.onrender.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+    host: '0.0.0.0',  // Optional: Use if you need access from other devices on the same network
+  },
+});
