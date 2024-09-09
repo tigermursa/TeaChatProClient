a8869dd0f90e71a6b6d48f0d4ba59477cb2cbe94
Mursalin <mursalin2311@gmail.com>
2024-09-10 00:29:02 +0600

    updated the titles

--- src/Components/PagesComponents/LoginPage/LoginTitle.jsx
+++ src/Components/PagesComponents/LoginPage/LoginTitle.jsx
@@ -7,8 +7,8 @@ const LoginTitle = () => {
       <h3 className="font-bold text-2xl flex items-center gap-1 md:text-4xl text-primary text-center mx-auto justify-center">
         Tea Chat <GiCoffeeCup className="text-yellow-700" />
       </h3>
-      <p className="font-semibold mt-2 text-sm text-slate-300 text-center">
-        "Connects people and fosters sharing"
+      <p className="font-semibold font-mono mt-2 text-sm text-slate-300 text-center">
+        "Positive online interactions can boost mental well-being"
       </p>
     </div>
   );
--- src/Components/PagesComponents/SignUpPage/SignUPTitle.jsx
+++ src/Components/PagesComponents/SignUpPage/SignUPTitle.jsx
@@ -7,8 +7,8 @@ const SignUpTitle = () => {
       <h3 className="font-bold text-2xl md:text-4xl flex justify-center md:justify-start items-center gap-2 text-primary">
         Tea Chat <GiCoffeeCup className="text-yellow-700" />
       </h3>
-      <p className="font-semibold mt-2 text-sm md:text-base text-slate-300">
-        "Teachat facilitates connections and encourages sharing"
+      <p className="font-semibold mt-2 text-sm md:text-base text-slate-300 font-mono">
+        "Engaging content can educate and entertain"
       </p>
     </div>
   );
