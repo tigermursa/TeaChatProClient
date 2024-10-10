b7b97f65c12aec19206c64c040e91d7daac3af6e
Mursalin <mursalin2311@gmail.com>
2024-10-10 20:02:28 +0600

    updated people page

--- src/Components/PagesComponents/PeopleYouMayKnow/PYMKMobile.jsx
+++ src/Components/PagesComponents/PeopleYouMayKnow/PYMKMobile.jsx
@@ -6,9 +6,6 @@ import {
 } from "../../../redux/features/friend/friendApi";
 import { toast } from "react-toastify";
 import { useSocket } from "../../../Providers/SocketProvider";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 import Loader from "../../SmallComponents/Loader/Loader";
 
 const PYMKMobile = () => {
@@ -46,7 +43,7 @@ const PYMKMobile = () => {
 
   if (isFetching || isLoading) {
     return (
-      <div className="h-screen w-full bg-people-background  bg-cover bg-no-repeat bg-bottom">
+      <div className="h-screen w-full bg-people-background bg-cover bg-no-repeat bg-bottom">
         <div className="overlay h-full w-full flex justify-center items-center">
           <Loader />
         </div>
@@ -62,58 +59,39 @@ const PYMKMobile = () => {
     );
   }
 
-  const settings = {
-    dots: false,
-    infinite: data?.data?.length > 1, // Disable infinite scroll if only one item
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: data?.data?.length > 1, // Disable autoplay if only one item
-    autoplaySpeed: 3000,
-    pauseOnHover: true,
-    arrows: false,
-    centerMode: false,
-    vertical: false,
-    swipeToSlide: true,
-  };
-
   return (
     <div className="bg-people-background bg-no-repeat bg-cover bg-bottom h-screen">
-      <div className="overlay mx-auto h-full">
-        <Slider {...settings} className="pt-5 ms-5 me-5">
-          {data?.data?.map((user) => (
-            <div
-              key={user?._id}
-              className="bg-white bg-opacity-0 border border-primary rounded-lg shadow-md overflow-hidden transform transition-all duration-500 hover:shadow-lg"
-            >
-              <div className="pt-5 bg-gray-200 bg-opacity-0 flex items-center justify-center">
-                <Img
-                  src={user?.userImage}
-                  alt={user?.username}
-                  className="w-60 h-60 object-fill bg-center rounded-full"
-                />
-              </div>
-              <div className="p-4 text-center">
-                <p className="font-semibold text-gray-100 mb-3 truncate text-2xl font-sans">
-                  {user?.username}
-                </p>
-                <button
-                  onClick={() => handleSendFriendRequest(user?._id)}
-                  className={`w-full py-2 text-sm font-semibold rounded-md transition-colors duration-200 ${
-                    sentFriendRequestsArray.includes(user?._id)
-                      ? "bg-gray-400 text-gray-800 cursor-not-allowed"
-                      : "bg-primary bg-opacity-30 border border-primary text-white hover:bg-primary"
-                  }`}
-                  disabled={sentFriendRequestsArray.includes(user?._id)}
-                >
-                  {sentFriendRequestsArray.includes(user?._id)
-                    ? "Request Sent"
-                    : "Add Friend"}
-                </button>
-              </div>
+      <div className="overlay mx-auto h-full px-5 py-5">
+        {data?.data?.map((user) => (
+          <div
+            key={user?._id}
+            className="w-[250px] mx-auto flex items-center text-center  bg-white bg-opacity-10 border border-primary rounded-lg shadow-md mb-4 p-4"
+          >
+            <Img
+              src={user?.userImage}
+              alt={user?.username}
+              className="w-12 h-12 object-cover bg-center rounded-full"
+            />
+            <div className="ml-4 flex-1">
+              <p className="font-semibold text-gray-100 mb-2  text-lg truncate">
+                {user?.username}
+              </p>
+              <button
+                onClick={() => handleSendFriendRequest(user?._id)}
+                className={`w-[100px] py-1 text-sm font-semibold rounded-md transition-colors duration-200 ${
+                  sentFriendRequestsArray.includes(user?._id)
+                    ? "bg-gray-400 text-gray-800 cursor-not-allowed"
+                    : "bg-primary bg-opacity-30 border border-primary text-white hover:bg-primary"
+                }`}
+                disabled={sentFriendRequestsArray.includes(user?._id)}
+              >
+                {sentFriendRequestsArray.includes(user?._id)
+                  ? "Request Sent"
+                  : "Add Friend"}
+              </button>
             </div>
-          ))}
-        </Slider>
+          </div>
+        ))}
       </div>
     </div>
   );
